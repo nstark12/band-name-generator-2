e715b9f9874d64346e1587a6341b57f182a1dfbf
Marketing Director <nicolestark1103@gmail.com>
2023-07-31 11:42:09 -0500

    inquirer

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 import { generateSlug } from 'random-word-slugs';
 import chalkAnimation from 'chalk-animation';
+import inquirer from 'inquirer';
 
-const numWords = parseInt(process.argv[2]);
+inquirer.prompt([
+    {
+        type: 'number',
+        name: 'numWords',
+        message: 'How many words do you want in your band name?',
+        default: 3,
+    },
+    {
+        type: 'list',
+        name: 'animationName',
+        message: 'Which animation do you want to use?',
+        choices: ['rainbow', 'pulse', 'glitch', 'radar', 'neon', 'karaoke']
+    }
+])
+    .then((answers) => {
+        const randomSlug = generateSlug(answers.numWords, {
+            format: 'title'
+        })
 
-const randomSlug = generateSlug(numWords, {
-    format: 'title'
-});
+        chalkAnimation[answers.animationName](randomSlug)
+    })
+    .catch((err) => {
+        console.log(err)
+    })
 
-chalkAnimation.rainbow(randomSlug);
+
+
+
